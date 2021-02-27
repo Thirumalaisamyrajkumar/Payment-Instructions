dc6f7a5771f021c6a5cf0ad7d2f27a146007f544
Thirumalaisamyrajkumar <Thirumalaisamyrajkumar@outlook.com>
2021-02-27 13:05:21 -0500

    initial commit

---
+++ CreditCheck/src/main/java/com/payments/processing/creditcheck/CreditcheckApplication.java
@@ -0,0 +1,13 @@
+package com.payments.processing.creditcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditcheckApplication.class, args);
+    }
+
+}
---
+++ CreditCheck/src/test/java/com/payments/processing/creditcheck/CreditcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payments.processing.creditcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditcheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
